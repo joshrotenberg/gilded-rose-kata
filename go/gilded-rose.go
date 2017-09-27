8b6ac9cb032d7381a5e6fba96c2fb7ae366d5290
josh rotenberg <joshrotenberg@users.noreply.github.com>
2017-09-27 15:26:37 -0700

    updates so we can test a single item update

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Item represents an item in the inventory
 type Item struct {
 	name            string
 	sellIn, quality int
@@ -16,60 +17,65 @@ var items = []Item{
 	Item{"Conjured Mana Cake", 3, 6},
 }
 
-func main() {
-	fmt.Println("OMGHAI!")
-	// fmt.Print(items)
-	GlidedRose()
-}
+// UpdateItem updates the sell-in and quality for the given item, or it returns an error if something bad happened
+func UpdateItem(item *Item) error {
 
-func GlidedRose() {
-	for i := 0; i < len(items); i++ {
-
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
-			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
+	if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
+		if item.quality > 0 {
+			if item.name != "Sulfuras, Hand of Ragnaros" {
+				item.quality = item.quality - 1
 			}
 		}
-
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
-		}
-
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
+	} else {
+		if item.quality < 50 {
+			item.quality = item.quality + 1
+			if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+				if item.sellIn < 11 {
+					if item.quality < 50 {
+						item.quality = item.quality + 1
 					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
 				}
-			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
+				if item.sellIn < 6 {
+					if item.quality < 50 {
+						item.quality = item.quality + 1
+					}
 				}
 			}
 		}
 	}
 
+	if item.name != "Sulfuras, Hand of Ragnaros" {
+		item.sellIn = item.sellIn - 1
+	}
+
+	if item.sellIn < 0 {
+		if item.name != "Aged Brie" {
+			if item.name != "Backstage passes to a TAFKAL80ETC concert" {
+				if item.quality > 0 {
+					if item.name != "Sulfuras, Hand of Ragnaros" {
+						item.quality = item.quality - 1
+					}
+				}
+			} else {
+				item.quality = item.quality - item.quality
+			}
+		} else {
+			if item.quality < 50 {
+				item.quality = item.quality + 1
+			}
+		}
+	}
+	return nil
+}
+
+func main() {
+	fmt.Print(items)
+	for i := 0; i < len(items); i++ {
+		err := UpdateItem(&items[i])
+		if err != nil {
+			fmt.Println(err)
+
+		}
+	}
+	fmt.Print(items)
 }
